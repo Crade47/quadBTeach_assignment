2d2ec0c3fdf89745d42ae523b15bfe016a8a4c9f
Devansh <devanshsoni79@gmail.com>
2023-04-01 13:23:51 +0530

    Added a carousel

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,12 @@
+import Carousel from "framer-motion-carousel";
+
+export default function CarouselElement({image}:{image:string}) {
+  console.log(image);
+
+  return (
+    <>
+       
+        
+    </>
+  )
+}
--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import Carousel from "framer-motion-carousel";
 interface MovieData{
     id:number,
     name:string,
@@ -8,6 +8,7 @@ interface MovieData{
         original: string
     },
     summary:string
+    genres: string[]
 }
 
 type ShowData = {
@@ -19,7 +20,9 @@ type ShowData = {
             original: string
         },
         summary: string,
+        genres:string[]
         [key: string]: any // allows additional properties not defined in the interface
+        
     }
 }
 
@@ -28,26 +31,73 @@ const MainPage = () => {
     useEffect(() => {
         
         const getMovieData = async () => {
+            console.log("getting data...")
             const res = await fetch("https://api.tvmaze.com/search/shows?q=all");
             const data:ShowData[] = await res.json();
+            console.log(data);
             setMovieData(data.map((obj: ShowData)=>({
                 id:obj.show.id,
                 name:obj.show.name,
                 image: obj.show.image,
-                summary: obj.show.summary
+                summary: obj.show.summary,
+                genres: obj.show.genres
             })))
             
         }
 
         getMovieData();
-        
     }, [])
-
-    console.log(movieData);
+    console.log(movieData)
   return (
     <>
         <div className='bg-lightMain dark:bg-darkMain h-screen'>
-        
+            <div className='h-[600px]'>
+                <Carousel 
+                    interval={3000} autoPlay={true} loop={true}
+                    
+                >
+                    {
+                        movieData.map((obj,i)=>{
+                            if(obj.image){
+                                return(
+                                    <>
+                                        <img
+                                        draggable="false"
+                                        src={obj.image.original}
+                                        key={i}
+                                        width="100%"
+                                        alt={`${obj.name} carousel picture`}
+                                        className='overflow-hidden fixed left-0 top-0 opacity-60'
+                                        />
+                                        <h1 
+                                            className='
+                                            text-5xl font-redHat font-bold text-white
+                                            bottom-[10rem] left-7 absolute'
+                                            key={obj.name}
+                                        >
+                                                {obj.name}
+                                        </h1>
+                                        {
+                                           <h1 
+                                           key={obj.id}
+                                           className='
+                                           text-white 
+                                           bottom-[7rem] left-7 absolute 
+                                           '
+                                           >
+                                               {obj.genres[0]}
+                                           </h1>
+                                        }
+                                       
+                                    </>  
+                                )
+                            }else{
+                                return null
+                            }
+                        })
+                    }
+                </Carousel>
+            </div>
         </div>
     </>
   )
